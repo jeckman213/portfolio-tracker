6378b6c2d58a9664ab89b76b37e828fb0bb2398f
jeckman21 <jonathaneckman@yahoo.com>
2019-10-09 09:29:17 -0400

    Added React to project. Removed Jade as view engine. Express now just acts as a back-end api server. React runs on port 3000, while Express runs on port 5000.

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Sucessfully Connected to Express Server' });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
