f229eb313e95d1e4c19c750c61d642baa561462f
jeckman21 <jonathaneckman@yahoo.com>
2019-10-15 16:32:57 -0400

    Tied in stock api from WorldTradingData

--- client/src/components/app/App.jsx
+++ client/src/components/app/App.jsx
@@ -1,27 +1,25 @@
 import React, { Component } from 'react';
 
-import logo from './logo.png';
+import logo from '../../assets/logos/portfolios/logo.png';
 
 import './App.css';
 
 class App extends Component {
   state = {
-    response: '',
-    post: '',
-    responseToPost: '',
+    model: '',
   };
   
   componentDidMount() {
     this.callApi()
-      .then(res => this.setState({ response: res.express }))
+      .then(res => this.setState({ model: res }))
       .catch(err => console.log(err));
   }
 
   callApi = async () => {
-    const response = await fetch('/api/hello');
+    const response = await fetch('/api/stock/realtime/AAPL');
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
-    
+    console.log(body);
     return body;
   };
 
@@ -35,7 +33,10 @@ render() {
             Portfolio<span className="Dollar-Sign">$</span>
           </h2>
         </header>
-        <p>{this.state.response}</p>
+        <p><b>Name:</b> {this.state.model.name}</p>
+        <p><b>Price:</b> {this.state.model.price}</p>
+        <p><b>Day High:</b> {this.state.model.day_high}</p>
+        <p><b>Day Low:</b> {this.state.model.day_low}</p>
       </div>
     );
   }
