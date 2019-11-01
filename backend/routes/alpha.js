4d1b9c85fe8a5bbfb7df806867e8af8f681ef9c2
jeckman21 <36603515+jeckman21@users.noreply.github.com>
2019-11-01 14:31:17 -0400

    Made endpoint for alphavantage wrapper, some still incomplete

--- backend/routes/alpha.js
+++ backend/routes/alpha.js
@@ -2,13 +2,51 @@ const express = require('express'),
       router = express.Router(),
       alpha = require('../services/alpha-vantage');
 
-router.get('/intraday/:symbol', (req, res) => {
+router.get('/intraday/:symbol/:interval?', (req, res) => {
     const symbol = req.params.symbol;
+          interval = req.params.interval;
 
-    alpha.getIntraday(symbol, data => {
+    if (interval == null || interval == "") interval = '60min';
+
+    alpha.getIntraday(symbol, interval, data => {
         res.send(data);
     });
 });
+
+router.get('/daily/:symbol/:interval?', (req, res) => {
+    const symbol = req.params.symbol;
+          interval = req.params.interval;
+
+    alpha.getDailyAdjusted(symbol, interval, data => {
+        res.send(data);
+    });
+});
+
+router.get('/weekly/:symbol/:interval?', (req, res) => {
+    const symbol = req.params.symbol;
+          interval = req.params.interval;
+
+    alpha.getWeeklyAdjusted(symbol, interval, data => {
+        res.send(data);
+    });
+});
+
+router.get('/monthly/:symbol/:interval?', (req, res) => {
+    const symbol = req.params.symbol;
+          interval = req.params.interval;
+
+    alpha.getMonthlyAdjusted(symbol, interval, data => {
+        res.send(data);
+    });
+});
+
+router.get('/search/:keyword', (req, res) => {
+    const keyword = req.params.keyword;
+
+    alpha.stockSearch(keyword, data => {
+        res.send(data);
+    });
+})
 
 module.exports = router;
 
--- backend/services/alpha-vantage.js
+++ backend/services/alpha-vantage.js
@@ -1,9 +1,39 @@
-const alpha = require('alphavantage')({key: process.env.ALPHA_TOKEN});
+const alpha = require('alphavantage').default();
 
 var exports = module.exports = {};
 
-exports.getIntraday = (symbol, callback) => {
-    alpha.data.monthly_adjusted(symbol, 60).then(data => {
+exports.getIntraday = (symbol, interval = '60min', callback) => {
+    alpha.data.intraday(symbol).then(data => {
+        callback(data);
+    });
+}
+
+exports.getBatch = (symbols, callback) => {
+    alpha.data.batch(symbols).then(data => {
+        callback(data);
+    });
+}
+
+exports.getDailyAdjusted = (symbol, interval = "60min", callback) => {
+    alpha.data.daily_adjusted(symbol).then(data => {
+        callback(data);
+    });
+}
+
+exports.getWeeklyAdjusted = (symbol, interval, callback) => {
+    alpha.data.weekly_adjusted(symbol).then(data => {
+        callback(data);
+    });
+}
+
+exports.getMonthlyAdjusted = (symbol, interval, callback) => {
+    alpha.data.monthly_adjusted(symbol).then(data => {
+        callback(data);
+    });
+}
+
+exports.stockSearch = (keywords, callback) => {
+    alpha.data.search(keywords).then(data => {
         callback(data);
     });
 }
