83c870fd460f77f4f82fd66835cb1c005fea84a2
aDevCalledAlex <daphrat@gmail.com>
2019-11-14 19:02:11 -0500

    removed sold_at from asset

--- backend/db/seeders/20191108285612-assets.js
+++ backend/db/seeders/20191108285612-assets.js
@@ -2,90 +2,79 @@
 
 const seeds = {
   up : (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Asset', 
+    return queryInterface.bulkInsert('asset', 
     [
       {
-        stock_id : 4174, // GE
+        symbol : 'GE',
         portfolio_id : 1,
         shares : 100,
-        purchased_at : '2016-10-05',
-        sold_at : null
+        purchased_at : '2016-10-05'        
       },
       {
-        stock_id : 4174,
+        symbol : 'GE',
         portfolio_id : 2,
         shares : 12,
-        purchased_at : '2015-10-05',
-        sold_at : null
+        purchased_at : '2015-10-05'        
       },
       {
-        stock_id : 4174,
+        symbol : 'GE',
         portfolio_id : 3,
         shares : 13,
-        purchased_at : '2014-10-05',
-        sold_at : null
+        purchased_at : '2014-10-05'        
       },
       {
-        stock_id : 4174,
+        symbol : 'GE',
         portfolio_id : 4,
         shares : 21,
-        purchased_at : '2013-10-05',
-        sold_at : null
+        purchased_at : '2013-10-05'        
       },
       {
-        stock_id : 4174,
+        symbol : 'GE',
         portfolio_id : 5,
         shares : 23,
-        purchased_at : '2012-10-05',
-        sold_at : null
+        purchased_at : '2012-10-05'        
       },
       {
-        stock_id : 4174,
+        symbol : 'GE',
         portfolio_id : 6,
         shares : 43,
-        purchased_at : '2011-10-05',
-        sold_at : null
+        purchased_at : '2011-10-05'        
       },
       {
-        stock_id : 4174,
+        symbol : 'GE',
         portfolio_id : 7,
         shares : 60,
-        purchased_at : '2010-10-05',
-        sold_at : null
+        purchased_at : '2010-10-05'        
       },
       {
-        stock_id : 6, // AAPL
+        symbol : 'AAPL',
         portfolio_id : 1,
         shares : 8,
-        purchased_at : '2017-9-05',
-        sold_at : null
+        purchased_at : '2017-09-05'        
       },
       {
-        stock_id : 1182, // GOOG
+        symbol : 'GOOG',
         portfolio_id : 1,
         shares : 11,
-        purchased_at : '2017-8-05',
-        sold_at : null
+        purchased_at : '2017-08-05'        
       },
       {
-        stock_id : 157, // AMZN
+        symbol : 'AMZN',
         portfolio_id : 1,
         shares : 14,
-        purchased_at : '2017-7-05',
-        sold_at : null
+        purchased_at : '2017-07-05'        
       },
       {
-        stock_id : 1776, // MSFT
+        symbol : 'MSFT',
         portfolio_id : 1,
         shares : 22,
-        purchased_at : '2017-6-05',
-        sold_at : null
+        purchased_at : '2017-06-05'        
       }
     ], {});
   },
 
   down : (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Asset', null, {});
+    return queryInterface.bulkDelete('asset', null, {});
   }
 };
 
