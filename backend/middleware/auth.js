2de1e44e17a32e93ac364200b3a69b8e33815f21
aDevCalledAlex <daphrat@gmail.com>
2019-11-25 21:34:58 -0500

    moved auth middleware to separate file

---
+++ backend/middleware/auth.js
@@ -0,0 +1,26 @@
+const
+  { sendExpectedError } = require('../services/responses'),
+  middleware = {};
+
+// Checks if a user is authenticated by passport
+middleware.isAuthenticated = (req, res, next) => {
+  req.isAuthenticated() 
+    ? next()
+    : sendExpectedError('User not authenticated', res, 401);
+};
+
+// Checks if the user is authenticated by passport
+// and is authorized to access the resource specified
+// by the endpoint's params (userId, portfolioId, assetId)
+middleware.isAuthorized = (req, res, next) => {
+  if(req.isAuthenticated()){
+    const userId = req.params.userId;
+
+    req.user.id == userId 
+      ? next()
+      : sendExpectedError('Unauthorized access', res, 403);
+  }
+  else { sendExpectedError('User not authenticated', res, 401); }
+}
+
+module.exports = middleware;
--- backend/middleware/index.js
+++ backend/middleware/index.js
@@ -1,32 +1,7 @@
 const
-  { expectedError, unexpectedError } = require('../services/errorhandling'),
+  authMiddleware = require('./auth'),
   portfolioMiddleware = require('./portfolio'),
   assetMiddleware = require('./asset'),
-  middleware = {...portfolioMiddleware, ...assetMiddleware};
-
-// Checks if a user is authenticated by passport
-middleware.isAuthenticated = (req, res, next) => {
-  req.isAuthenticated() 
-    ? next()
-    : res.send(expectedError('User not authenticated', res, 401));
-};
-
-// middleware.isNotAuthenticated = (req, res, next) => {
-
-// };
-
-// Checks if the user is authenticated by passport
-// and is authorized to access the resource specified
-// by the endpoint's params (userId, portfolioId, assetId)
-middleware.isAuthorized = (req, res, next) => {
-  if(req.isAuthenticated()){
-    const { userId } = req.params;
-
-    req.user.id == userId 
-      ? next()
-      : res.send(expectedError('Unauthorized access', res, 403));
-  }
-  else { res.send(expectedError('User not authenticated', res, 401)); }
-}
+  middleware = {...authMiddleware, ...portfolioMiddleware, ...assetMiddleware};
 
 module.exports = middleware;
